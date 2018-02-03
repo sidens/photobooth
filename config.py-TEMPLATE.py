e644d7c345151c33142f7554e072345845d94408
sidens <sidens@gmail.com>
2018-02-03 16:43:07 -0500

    new config template

---
+++ config.py-TEMPLATE.py
@@ -0,0 +1,15 @@
+# Copy this file to config.py and replace the values with your information
+# OAuth keys can be generated from https://api.tumblr.com/console/calls/user/info
+tumblr_consumer_key='' #replace with your key
+tumblr_consumer_secret='' #replace with your secret code
+tumblr_oath_token='' #replace with you oath token
+tumblr_oath_secret='' #replace with your oath secret code
+twitter_CONSUMER_KEY = ''
+twitter_CONSUMER_SECRET = ''
+twitter_ACCESS_KEY = ''
+twitter_ACCESS_SECRET = ''
+file_path = '/home/pi/photobooth/pics/' # path to save images
+
+#Event specific Config
+tumblr_blog = '' # replace with your tumblr account name without .tumblr.com
+event_tag = 'piboothtest'
